5b0c52fb22f733e0cae3d5e0d3a0ae3d42a545c0
Allan Armstrong <allan@hit3k.net>
2015-11-03 14:05:41 +1100

    Starting again

--- server/app.js
+++ server/app.js
@@ -1,19 +1,36 @@
+/**
+ * Main application file
+ */
+
+'use strict';
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var express = require('express');
-var path = require('path');
-var config = require('./config/environment/index');
+var mongoose = require('mongoose');
+var config = require('./config/environment');
 
+// Connect to database
+mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connection.on('error', function(err) {
+	console.error('MongoDB connection error: ' + err);
+	process.exit(-1);
+	}
+);
+// Populate DB with sample data
+if(config.seedDB) { require('./config/seed'); }
 
-var routes = require('./routes');
-
-
+// Setup server
 var app = express();
-
-
 var server = require('http').createServer(app);
 require('./config/express')(app);
-routes(app);
+require('./routes')(app);
 
-
-app.listen(config.port, function() {
-	console.log("Listening on port " + config.port);
+// Start server
+server.listen(config.port, config.ip, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
+
+// Expose app
+exports = module.exports = app;
--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -1,13 +1,53 @@
+'use strict';
+
 var path = require('path');
 var _ = require('lodash');
-var settings = require('../settings');
 
+function requiredProcessEnv(name) {
+  if(!process.env[name]) {
+    throw new Error('You must set the ' + name + ' environment variable');
+  }
+  return process.env[name];
+}
+
+// All configurations will extend these options
+// ============================================
 var all = {
+  env: process.env.NODE_ENV,
 
-	port: process.env.port || 3000,
+  // Root path of server
+  root: path.normalize(__dirname + '/../../..'),
 
-	root: path.normalize(__dirname + '/../../..'),
+  // Server port
+  port: process.env.PORT || 9000,
+
+  // Server IP
+  ip: process.env.IP || '0.0.0.0',
+
+  // Should we populate the DB with sample data?
+  seedDB: false,
+
+  // Secret for session, you will want to change this and make it an environment variable
+  secrets: {
+    session: 'findabar-secret'
+  },
+
+  // List of user roles
+  userRoles: ['guest', 'user', 'admin'],
+
+  // MongoDB connection options
+  mongo: {
+    options: {
+      db: {
+        safe: true
+      }
+    }
+  },
+
 };
+
+// Export the config object based on the NODE_ENV
+// ==============================================
 module.exports = _.merge(
   all,
   require('./' + process.env.NODE_ENV + '.js') || {});
