e2ba07e77a044a4e4c8b0fc1ca2081dab52f4633
Allan Armstrong <allan@hit3k.net>
2015-11-03 14:06:26 +1100

    Starting again

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('findabarApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
